e0eef3090ce4bf03605de176ffcb7c36695d9aae
ferfsnandes <112121591+ferfsnandes@users.noreply.github.com>
2024-10-24 19:14:14 -0300

    Estrutura Basica + funcionalidades Iniciais funcionado
    Interface grafica inicial basica sem muitos detalhes + operaçôes iniciais de manipulação(criar,excluir e redimencionar aqruivos) funcionando

--- untitled/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled_FILE C:/Users/gafgo/Documents/GitHub/Sistema_de_arquivos/untitled/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/untitled.exe)
-set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
